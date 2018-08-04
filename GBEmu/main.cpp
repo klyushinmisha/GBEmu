eb7161c5da82c0a9efe01abcbb3e3533e5d80052
Михаил <mikhail@Mihails-iMac.local>
2018-08-04 15:36:41 +0300

    Initial Commit

---
+++ GBEmu/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GBEmu
+//
+//  Created by Михаил on 04.08.18.
+//  Copyright © 2018 Михаил. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
